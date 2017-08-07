371316a845ce738ef93327547bef05d5fa9fc8bd
wing <wing@listcloud.cn>
2017-08-08 05:34:58 +0800

    adjust

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-#基础镜像maven
 FROM daocloud.io/java
-#镜像创建者信息
-MAINTAINER moses
-#复制项目代码
+
 ADD ./ /home/logstash
+
 #切换目录启动脚本
 WORKDIR /home/logstash
-#公布tomcat的8080端口
+
 EXPOSE 9600
+
 #设置启动后执行的操作
 ENTRYPOINT ["/home/logstash/bin/logstash", "-f", "/home/logstash/config/logstash_shipper.conf"]
--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
-#构建
-docker build -t 127.0.0.1:5000/logstash --rm=true .
-#启动
-docker run --name=logstash -p 9600:9600 -v /home/docker/log/nginx:/home/docker/log/nginx -ti -d 127.0.0.1:5000/logstash
+docker rm -f logstash
 docker run --restart=always --name=logstash -p 9600:9600 -v /home/logstash1/config:/home/logstash/config -v /home/logstash1/logs:/home/logstash/logs -v /home/docker/log/nginx:/home/docker/log/nginx -ti -d 127.0.0.1:5000/logstash
